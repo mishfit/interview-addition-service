516922100cd29911075501ccda46ee206f367add
RohitPawar11 <107909805+RohitPawar11@users.noreply.github.com>
2025-12-31 21:49:21 +0530

    Update AdditionController.java

--- src/main/java/com/interview/addition/controller/AdditionController.java
+++ src/main/java/com/interview/addition/controller/AdditionController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.*;
 
 @RestController
 @RequestMapping("/api")
 public class AdditionController {
 
     private final CalculatorService calculatorService;
+    private static final Logger logger = LoggerFactory.getLogger(AdditionController.class);
+
 
     public AdditionController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
@@ -22,6 +25,7 @@ public class AdditionController {
     @PostMapping("/add")
     public ResponseEntity<AdditionResponse> add(@RequestBody AdditionRequest request) {
         Double result = calculatorService.add(request.getNumber1(), request.getNumber2());
+        logger.info("Result :- "+result);
         return ResponseEntity.ok(new AdditionResponse(result));
     }
 }
