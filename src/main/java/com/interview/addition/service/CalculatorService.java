e3ed06cdbbba3f11b7dfbb15b426bcb0abb5c424
RohitPawar11 <107909805+RohitPawar11@users.noreply.github.com>
2025-12-31 21:48:56 +0530

    Add logging to CalculatorService add method

--- src/main/java/com/interview/addition/service/CalculatorService.java
+++ src/main/java/com/interview/addition/service/CalculatorService.java
@@ -1,9 +1,12 @@
 package com.interview.addition.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+    private static final Logger logger = LoggerFactory.getLogger(CalculatorService.class);
 
     /**
      * add two numbers
@@ -15,11 +18,13 @@ public class CalculatorService {
     public Double add(Double number1, Double number2) {
         try
         {
+            logger.info("Adding number1: " + number1 + " and number2: " + number2);
             return number1 + number2;
         }
 
         catch (ArithmeticException ae)
         {
+            logger.error("ArithmeticException :- "+ae.getMessage());
             throw new ArithmeticException(ae.getMessage());
         }
     }
